74aa150b62e4c4a11a2144566ffd7be83b219f7c
Narzaru <yaroslahek@gmail.com>
2021-03-19 23:41:38 +0700

    fixed logic mistakes

--- settingsClass.py
+++ settingsClass.py
@@ -6,9 +6,9 @@ class JsonShell():
     def __init__(self, filePath=str, encoding='utf-8'):
         self.filePath = filePath
         self.encoding = encoding
-        self.data = self.open()
+        self.data = self.__open()
 
-    def open(self):
+    def __open(self):
         try:
             with open(file=self.filePath, mode='r', encoding=self.encoding) as file:
                 return json.load(file)
@@ -20,7 +20,7 @@ class JsonShell():
             return None
 
     def dump(self, filePath=None):
-        if filePath is not None:
+        if filePath is None:
             filePath = self.filePath
         with open(file=filePath, mode='w', encoding='utf-8') as file:
             json.dump(self.data, file, indent=4, ensure_ascii=False)
@@ -30,43 +30,3 @@ class JsonShell():
 
     def put(self, data=str):
         self.data = data
-
-
-'''{
-    "599993492382416907": {
-        "WELCOME_CHANNEL_ID": "599993493208956930",
-        "WELCOME_CHANNEL_REACTION": [
-            ", добро пожаловать, сенпай !",
-            ", приятно познакомиться~!   ",
-            ", чувствуй себя как дома!   ",
-            ", осматривайся и знакомься~ "
-        ],
-        "RULE_MESSAGE_ID": "762339000676909066",
-        "RULE_ACCEPT_EMOJI": "✅",
-        "RULE_ACCEPT_ROLE": "625711440975626282",
-        "ROLE_MESSAGE_ID": "770836261879545897",
-        "ROLE_BY_EMOJI": {
-            "🔪": "760832315466711070",
-            "🔫": "738066613621882942",
-            "↗️": "738005330990071858",
-            "🏹": "731421506961408060",
-            "🧠": "761997590068723743",
-            "⚓": "761999284693368842",
-            "🦿": "770834462514741258"
-        },
-        "EXCLUDE_ROLES": [
-            600195592446017536,
-            600021636942397484,
-            600020445160538112,
-            600020896933085304,
-            600021126667698261,
-            625711440975626282,
-            599993492382416907
-        ],
-        "MAX_ROLE_PER_USER": "24",
-        "ROLE_ADMIN": {
-            "NAME": "Admin",
-            "ID": "600021636942397484"
-        }
-    }
-}'''
